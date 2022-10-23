9548d919a07f3c87be2fa2eed96f5dd8c04658f1
Stanislav Yanakiev <stan5050@gmail.com>
2022-10-23 13:21:35 +0300

    Update admin.py

--- forms_demos_mine/departments/admin.py
+++ forms_demos_mine/departments/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from forms_demos_mine.departments.models import MuscleCar
 
 
+
 @admin.register(MuscleCar)
 class MuscleCarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'modification', 'horse_powers', 'slug',)
